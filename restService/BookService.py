84b797e9e667267629517cc88cae443920b909c0
akliuieva <akliuieva@griddynamics.com>
2020-02-04 17:23:23 +0100

    Changes according to code review

--- restService/BookService.py
+++ restService/BookService.py
@@ -1,33 +1,13 @@
-import uuid
-from enum import Enum
+#!/usr/bin/env python3
 from flask import Flask, jsonify, make_response, request
 from datetime import date, datetime
 import logging
+import configparser
 
-app = Flask(__name__) #“__name__” is a Python special variable which gives Python file a unique name
+from Book import Book, BookType
 
-class BookType(str, Enum):
-    SCIENCE_FICTION: str = "Science Fiction"
-    SATIRE: str = "Satire"
-    DRAMA: str = "Drama"
-    ACTION_AND_ADVENTURE: str = "Action and Adventure"
-    ROMANCE: str = "Romance"
+app = Flask(__name__)  # “__name__” is a Python special variable which gives Python file a unique name
 
-class Book:
-    def __init__(self, book_type, title, creation_date):
-        self._book_type = book_type
-        self._title = title
-        self._id = str(uuid.uuid4())
-        if creation_date is not None:
-                self._creation_date = creation_date
-        else:
-            self._creation_date = None
-
-        self._updated_date_time = datetime.now().isoformat()
-
-    def json(self):
-        return {"_book_type":self._book_type, "_title":self._title, "_id":self._id, "_creation_date":self._creation_date,
-                "_updated_date_time":self._updated_date_time}
 
 def set_books():
     book1 = Book(BookType.ACTION_AND_ADVENTURE, "The Fountainhead", date(1943, 10, 1).isoformat())
@@ -35,46 +15,79 @@ def set_books():
     book3 = Book(BookType.SATIRE, "Animal Farm", None)
     return [book1, book2, book3]
 
+
 global books
 books = set_books()
 
-def get_books():
-    return books
 
-# Returns “No implementation for GET method”
-@app.route("/v1/books/manipulation/", methods = ['GET'])
-def get_book_manipulation ():
-        return "No implementation for GET method"
-
-def validateObject(bookObject):
-    if "_book_type" in bookObject and "_title" in bookObject and "_creation_date" in bookObject:
-        listEnumValues = [e.value for e in BookType]
-        logging.info("Book type: "+str(bookObject["_book_type"]))
-        if str(bookObject["_book_type"]) in listEnumValues:
-            if len(bookObject["_title"]) < 256 and len(bookObject["_title"])>0:
-                logging.info("Input data has valid book type and valid title")
-                try:
-                    date.fromisoformat(bookObject["_creation_date"])
-                except:
-                    logging.error("Input data has incorrect creation date")
-                    return False
-                return True
-            else:
-                logging.error("Title is more than 256 symbols or empty")
-                return False
-        else:
-            logging.error("Book type is out of available values:"+str(listEnumValues))
-            return False
+def is_book_type_has_valid_value(income_book_type):
+    list_enum_values = [e.value for e in BookType]
+    logging.info("Book type: " + str(income_book_type))
+    if str(income_book_type) in list_enum_values:
+        return True
     else:
-        logging.error("Input data doesn't contain obligatory property")
+        logging.error("Book type is out of available values:" + str(list_enum_values))
         return False
 
-# Add book with no arguments but request payload as json contains fields (type, title, creation date)
-@app.route("/v1/books/manipulation/", methods = ['POST'])
+
+def is_title_has_valid_lengths(income_title):
+    if 256 > len(income_title) > 0:
+        logging.info("Input data has valid book type and valid title")
+        return True
+    else:
+        logging.error("Title is more than 256 symbols or empty")
+        return False
+
+
+def is_date_has_valid_format(income_date):
+    try:
+        date.fromisoformat(income_date)
+    except:
+        logging.error("Input data has incorrect creation date")
+        return False
+    return True
+
+
+def has_correct_values(income_data):
+    if is_book_type_has_valid_value(income_data["_book_type"]) and is_title_has_valid_lengths(
+            income_data["_title"]) and is_date_has_valid_format(income_data["_creation_date"]):
+        return True
+    else:
+        return False
+
+
+def validate_object(bookObject):
+    if "_book_type" in bookObject and "_title" in bookObject and "_creation_date" in bookObject:
+        return has_correct_values(bookObject)
+    else:
+        logging.error("Input data doesn't contain obligatory property")
+    return False
+
+
+@app.route("/v1/books/manipulation/", methods=['GET'])
+def get_book_manipulation():
+    """Stub method that that returns "No implementation for GET method"
+        Args:
+            no args
+
+        Return:
+            "No implementation for GET method" string
+        """
+    return "No implementation for GET method"
+
+
+@app.route("/v1/books/manipulation/", methods=['POST'])
 def post_book_manipulation():
+    """Create new book with income data
+        Args:
+            no args
+
+        Return:
+            Json with new book
+        """
     request_data = request.get_json()
     logging.info("Data from request: " + str(request_data))
-    if validateObject(request_data):
+    if validate_object(request_data):
         book = Book(request_data["_book_type"], request_data["_title"], request_data["_creation_date"])
         logging.info("Book to be posted: " + str(book.json()))
         books.append(book)
@@ -82,12 +95,19 @@ def post_book_manipulation():
     else:
         return "Incorrect input data", 400
 
-# Change the name of the book with arguments (id)
-@app.route("/v1/books/manipulation/<string:id>", methods = ['PUT'])
-def update_book_manipulation_by_id (id):
+
+@app.route("/v1/books/manipulation/<string:id>", methods=['PUT'])
+def update_book_manipulation_by_id(id):
+    """Update the book with income data
+    Args:
+        id:(string) The id of the book
+
+    Return:
+        Json with updated data
+    """
     request_data = request.get_json()
-    logging.info("Data from request: "+str(request_data))
-    if validateObject(request_data):
+    logging.info("Data from request: " + str(request_data))
+    if validate_object(request_data):
         for book in books:
             if book._id == id:
                 logging.info("Book with id {} already exists".format(id))
@@ -98,53 +118,85 @@ def update_book_manipulation_by_id (id):
                 return make_response(book.json(), 200)
 
         book = Book(request_data["_book_type"], request_data["_title"], request_data["_creation_date"])
-        logging.info("Book to be posted: "+str(book.json()))
+        logging.info("Book to be posted: " + str(book.json()))
         return make_response(book.json(), 201)
     else:
         return "Incorrect input data", 400
 
-# Delete book with arguments (id)
-@app.route("/v1/books/manipulation/<string:id>", methods = ['DELETE'])
-def delete(id):
-        for book in books:
-            if book._id == id:
-                books.remove(book)
-                logging.info("Book was deleted " + str(book.json()))
-                return "Book with id {} was deleted".format(id), 200
-        return "Book with id {} was not found".format(id), 404
 
-# Get all the latest added books limited by some amount with arguments (limit)
-@app.route("/v1/books/latest/<int:limit>", methods = ['GET'])
-def get_last_books (limit):
-    """Get all the latest added books limited by some amount with arguments (limit)"""
+@app.route("/v1/books/manipulation/<string:id>", methods=['DELETE'])
+def delete(id):
+    """Delete the book with specific id
+    Args:
+        id:(string) The id of the book
+
+    Return:
+        Response status equals to 200 if  the book was deleted successfully
+        Response status equals to 404 if the book was non deleted
+    """
+    for book in books:
+        if book._id == id:
+            books.remove(book)
+            logging.info("Book was deleted " + str(book.json()))
+            return "Book with id {} was deleted".format(id), 200
+    return "Book with id {} was not found".format(id), 404
+
+
+@app.route("/v1/books/latest/<int:limit>", methods=['GET'])
+def get_last_books(limit):
+    """Get all the latest added books limited by some amount with arguments (limit)
+    Args:
+        limit:(int) The quantity of the latest added books
+
+    Return:
+        Json with list of books
+    """
     books.sort(key=lambda x: x._updated_date_time, reverse=True)
     new_books = []
     for i in range(len(books) if len(books) < limit else limit):
-            new_books.append(books[i])
+        new_books.append(books[i])
 
     return make_response(jsonify([book.json() for book in new_books]), 200)
 
-@app.route("/v1/books/info/<string:id>", methods = ['GET'])
-def get_book_by_id (id):
-        """Get info about a book with arguments (ID)"""
-        for book in books:
-            if book._id == id:
-                return make_response(jsonify(book.json()), 200)
-        return "Book is not found", 404
 
-@app.route("/v1/books/ids/<string:title>", methods = ['GET'])
-def get_all_ids_for_books_with_title (title):
-        """Get all ID of books by title with arguments (title)"""
-        list_with_ids = []
-        for book in books:
-            if book._title == title:
-                list_with_ids.append(book._id)
+@app.route("/v1/books/info/<string:id>", methods=['GET'])
+def get_book_by_id(id):
+    """Get info about a book with arguments (ID)
+    Args:
+        id:(string) The id of the book
 
-        if len(list_with_ids) == 0:
-            return make_response("No books with title "+title, 404)
-        return make_response(jsonify(list_with_ids), 200)
+    Return:
+        Json with book with id mentioned as parameter
+    """
+    for book in books:
+        if book._id == id:
+            return make_response(jsonify(book.json()), 200)
+    return "Book is not found", 404
+
+
+@app.route("/v1/books/ids/<string:title>", methods=['GET'])
+def get_all_ids_for_books_with_title(title):
+    """Get all ID of books by title with arguments (title)
+    Args:
+        title:(string) The title of the book
+
+    Return:
+        Json with list of books
+    """
+    list_with_ids = []
+    for book in books:
+        if book._title == title:
+            list_with_ids.append(book._id)
+
+    if len(list_with_ids) == 0:
+        return make_response("No books with title " + title, 404)
+    return make_response(jsonify(list_with_ids), 200)
+
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
-    app.run(port=5000, debug=True)
-
+    config = configparser.ConfigParser()
+    config.read('configuration.ini')
+    port_number = int(config['flask.server']['Port'])
+    host = config['flask.server']['Host']
+    app.run(host=host, port=port_number)
--- restService/BookServiceTest.py
+++ restService/BookServiceTest.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+import configparser
 import logging
 
 import pytest
@@ -5,32 +7,48 @@ import pytest
 from hamcrest import assert_that, equal_to
 from requests import get, post, put, delete
 
-server_url = "http://127.0.0.1"
-server_host = "5000"
 
 def setup_module():
     logging.basicConfig(level=logging.INFO)
     logging.info("module setup")
+    global server_url, server_host
+    server_url = get_server_url()
+    server_host = get_server_port()
+
+
+def get_server_url():
+    config = configparser.ConfigParser()
+    config.read('configuration.ini')
+    return "http://" + config['flask.server']['Host']
+
+
+def get_server_port():
+    config = configparser.ConfigParser()
+    config.read('configuration.ini')
+    return config['flask.server']['Port']
+
 
 def teardown_module():
     logging.info("module teardown")
+
 
 @pytest.mark.post
 @pytest.mark.smoke
 @pytest.mark.positive
 def test_create_book_with_valid_data_return_201():
     new_book = {
-        "_book_type":"Romance",
-        "_title":"Bridget Jones's Diary",
-        "_creation_date":"1996-01-12"
+        "_book_type": "Romance",
+        "_title": "Bridget Jones's Diary",
+        "_creation_date": "1996-01-12"
     }
-    url_for_create_book = server_url+":"+server_host+"/v1/books/manipulation/"
+    url_for_create_book = server_url + ":" + server_host + "/v1/books/manipulation/"
     response = post(url_for_create_book, json=new_book)
 
-    assert_that(response.status_code, equal_to( 201))
-    assert_that(response.json()["_title"] == "Bridget Jones's Diary")
-    assert_that(response.json()["_book_type"] == "Romance")
-    assert_that(response.json()["_creation_date"] == "1996-01-12")
+    assert_that(response.status_code, equal_to(201))
+    assert_that(response.json()["_title"], equal_to("Bridget Jones's Diary"))
+    assert_that(response.json()["_book_type"], equal_to("Romance"))
+    assert_that(response.json()["_creation_date"], equal_to("1996-01-12"))
+
 
 @pytest.mark.post
 @pytest.mark.smoke
@@ -43,9 +61,10 @@ def test_create_book_with_invalid_quantity_of_parameters_return_400():
     url_for_create_book = server_url + ":" + server_host + "/v1/books/manipulation/"
     response = post(url_for_create_book, json=new_book)
 
-    assert_that( response.status_code, equal_to( 400))
-    assert_that( str(response.reason), equal_to("BAD REQUEST" ))
-    assert_that( response.text == "Incorrect input data" )
+    assert_that(response.status_code, equal_to(400))
+    assert_that(str(response.reason), equal_to("BAD REQUEST"))
+    assert_that(response.text, equal_to("Incorrect input data"))
+
 
 @pytest.mark.post
 @pytest.mark.smoke
@@ -55,37 +74,39 @@ def test_create_book_with_invalid_value_of_title_return_400():
                         NLBRNw0FiNdoTQ0eGeVSPYhbRAc6eLXYPeeirnmvEZP3dsiei7zrS9GRplvlVWvhQyzHwy9YHbfayZvK083yfQG3UfTC
                         JazmckgUzrtlA6GTHstHp0AoHJLo8ld6NnEtCQcbDdJR3XYp5NR4fNPOoZ3Ku9hiLeRD7boLe13""";
     new_book = {"_book_type": "Romance",
-                "_title":incorrect_title,
+                "_title": incorrect_title,
                 "_creation_date": "1996-01-12"
                 }
     url_for_create_book = server_url + ":" + server_host + "/v1/books/manipulation/"
     response = post(url_for_create_book, json=new_book)
 
-    assert_that( response.status_code, equal_to( 400 ))
-    assert_that( response.reason == "BAD REQUEST" )
-    assert_that( response.text == "Incorrect input data" )
+    assert_that(response.status_code, equal_to(400))
+    assert_that(response.reason, equal_to("BAD REQUEST"))
+    assert_that(response.text, equal_to("Incorrect input data"))
+
 
 @pytest.mark.get
 @pytest.mark.smoke
 @pytest.mark.positive
 def test_get_last_book():
-    #Pre-condition
+    # Pre-condition
     new_book = {"_book_type": "Science Fiction",
                 "_title": "Brave New World",
                 "_creation_date": "1932-01-12"
                 }
     url_for_create_book = server_url + ":" + server_host + "/v1/books/manipulation/"
-    logging.info("URl for post is :"+url_for_create_book)
+    logging.info("URl for post is :" + url_for_create_book)
     response = post(url_for_create_book, json=new_book)
-    assert response.status_code == 201
+    assert_that(response.status_code, equal_to(201))
 
-    #Main part
-    limit=1
-    url = server_url+":"+server_host+"/v1/books/latest/" + str(limit)
+    # Main part
+    limit = 1
+    url = server_url + ":" + server_host + "/v1/books/latest/" + str(limit)
     response = get(url)
     logging.info("URl for GET request is :" + url)
-    assert_that (response.status_code, equal_to( 200))
-    assert_that(response.json()[0]["_title"], equal_to( "Brave New World"))
+    assert_that(response.status_code, equal_to(200))
+    assert_that(response.json()[0]["_title"], equal_to("Brave New World"))
+
 
 @pytest.mark.get
 @pytest.mark.smoke
@@ -93,38 +114,15 @@ def test_get_last_book():
 def test_get_manipulation_book_has_valid_response_message():
     url = server_url + ":" + server_host + "/v1/books/manipulation/"
     response = get(url)
-    assert_that( response.status_code, equal_to( 200) )
-    assert_that( response.text, "No implementation for GET method")
+    assert_that(response.status_code, equal_to(200))
+    assert_that(response.text, "No implementation for GET method")
+
 
 @pytest.mark.get
 @pytest.mark.smoke
 @pytest.mark.positive
 def test_get_ids_for_book_with_exist_title_return_200():
-    #Pre-condition
-    new_book = {"_book_type": "Drama",
-                "_title": "The Hunger Games",
-                "_creation_date": "2008-01-12"
-                }
-    url_for_create_book = server_url + ":" + server_host + "/v1/books/manipulation/"
-    logging.info("URl for post is :"+url_for_create_book)
-    response = post(url_for_create_book, json=new_book)
-    assert response.status_code == 201
-
-    #Main part
-    expected_id = response.json()["_id"]
-    title = response.json()["_title"]
-    url = server_url + ":" + server_host + "/v1/books/ids/{}".format(title)
-    logging.info("URl for GET is :" + url)
-    response = get(url)
-
-    assert_that(response.status_code, equal_to( 200))
-    assert_that(expected_id in response.json())
-
-@pytest.mark.put
-@pytest.mark.smoke
-@pytest.mark.positive
-def test_update_existed_book_with_valid_data_return_200():
-    #Pre-condition
+    # Pre-condition
     new_book = {"_book_type": "Drama",
                 "_title": "The Hunger Games",
                 "_creation_date": "2008-01-12"
@@ -132,40 +130,67 @@ def test_update_existed_book_with_valid_data_return_200():
     url_for_create_book = server_url + ":" + server_host + "/v1/books/manipulation/"
     logging.info("URl for post is :" + url_for_create_book)
     response = post(url_for_create_book, json=new_book)
-    assert response.status_code == 201
+    assert_that(response.status_code, equal_to(201))
 
-    #Main part
+    # Main part
+    expected_id = response.json()["_id"]
+    title = response.json()["_title"]
+    url = server_url + ":" + server_host + "/v1/books/ids/{}".format(title)
+    logging.info("URl for GET is :" + url)
+    response = get(url)
+
+    assert_that(response.status_code, equal_to(200))
+    assert_that(expected_id in response.json())
+
+
+@pytest.mark.put
+@pytest.mark.smoke
+@pytest.mark.positive
+def test_update_existed_book_with_valid_data_return_200():
+    # Pre-condition
+    new_book = {"_book_type": "Drama",
+                "_title": "The Hunger Games",
+                "_creation_date": "2008-01-12"
+                }
+    url_for_create_book = server_url + ":" + server_host + "/v1/books/manipulation/"
+    logging.info("URl for post is :" + url_for_create_book)
+    response = post(url_for_create_book, json=new_book)
+    assert_that(response.status_code, equal_to(201))
+
+    # Main part
     id = response.json()["_id"]
     changed_book = new_book
-    changed_book["_book_type"]= "Action and Adventure"
+    changed_book["_book_type"] = "Action and Adventure"
     url_for_update_book = server_url + ":" + server_host + "/v1/books/manipulation/{}".format(id)
     logging.info("URl for PUT is :" + url_for_update_book)
     response = put(url_for_update_book, json=changed_book)
     assert_that(response.status_code, equal_to(200))
     assert_that(response.json()["_book_type"], equal_to(changed_book["_book_type"]))
 
+
 @pytest.mark.put
 @pytest.mark.smoke
 @pytest.mark.negative
 def test_update_existed_book_with_invalid_data_return_400():
-    #Pre-condition
+    # Pre-condition
     new_book = {"_book_type": "Drama",
                 "_title": "The Hunger Games",
                 "_creation_date": "2008-01-12"
                 }
     url_for_create_book = server_url + ":" + server_host + "/v1/books/manipulation/"
-    logging.info("URl for post is :"+url_for_create_book)
+    logging.info("URl for post is :" + url_for_create_book)
     response = post(url_for_create_book, json=new_book)
-    assert response.status_code == 201
+    assert_that(response.status_code, equal_to(201))
 
-    #Main part
+    # Main part
     id = response.json()["_id"]
     changed_book = new_book
-    changed_book["_book_type"]= "Action"
+    changed_book["_book_type"] = "Action"
     url_for_update_book = server_url + ":" + server_host + "/v1/books/manipulation/{}".format(id)
-    logging.info("URl for PUT is :"+url_for_update_book)
+    logging.info("URl for PUT is :" + url_for_update_book)
     response = put(url_for_update_book, json=changed_book)
     assert_that(response.status_code, equal_to(400))
+
 
 @pytest.mark.put
 @pytest.mark.smoke
@@ -177,7 +202,7 @@ def test_update_non_existed_book_return_201():
         "_creation_date": "2008-01-12"
     }
     url_for_update_book = server_url + ":" + server_host + "/v1/books/manipulation/{}".format("non_existing_id")
-    logging.info("URl for post is :"+url_for_update_book)
+    logging.info("URl for post is :" + url_for_update_book)
     response = put(url_for_update_book, json=book)
 
     assert_that(response.status_code, equal_to(201))
@@ -185,26 +210,28 @@ def test_update_non_existed_book_return_201():
     assert_that(response.json()["_book_type"], equal_to("Drama"))
     assert_that(response.json()["_creation_date"], equal_to("2008-01-12"))
 
+
 @pytest.mark.delete
 @pytest.mark.smoke
 @pytest.mark.positive
 def test_delete_existed_book_return_200():
-    #Pre-condition
+    # Pre-condition
     new_book = {"_book_type": "Drama",
                 "_title": "The Hunger Games",
                 "_creation_date": "2008-01-12"
                 }
     url_for_create_book = server_url + ":" + server_host + "/v1/books/manipulation/"
-    logging.info("URl for post is :"+url_for_create_book)
+    logging.info("URl for post is :" + url_for_create_book)
     response = post(url_for_create_book, json=new_book)
-    assert response.status_code == 201
+    assert_that(response.status_code, equal_to(201))
 
-    #Main part
+    # Main part
     id = response.json()["_id"]
     url_for_book_deletion = server_url + ":" + server_host + "/v1/books/manipulation/{}".format(id)
     logging.info("URl for DELETE is :" + url_for_book_deletion)
     response = delete(url_for_book_deletion)
     assert_that(response.status_code, equal_to(200))
+
 
 @pytest.mark.delete
 @pytest.mark.smoke
@@ -217,30 +244,32 @@ def test_delete_non_existed_book_return_404():
     assert_that(response.status_code, equal_to(404))
     assert_that(response.reason, equal_to("NOT FOUND"))
 
+
 @pytest.mark.get
 @pytest.mark.smoke
 @pytest.mark.positive
 def test_get_book_by_existing_id_return_200():
-    #Pre-condition
+    # Pre-condition
     new_book = {"_book_type": "Drama",
                 "_title": "Twilight",
                 "_creation_date": "2005-01-12"
                 }
     url_for_create_book = server_url + ":" + server_host + "/v1/books/manipulation/"
-    logging.info("URl for post is :"+url_for_create_book)
+    logging.info("URl for post is :" + url_for_create_book)
     response = post(url_for_create_book, json=new_book)
-    assert response.status_code == 201
+    assert_that(response.status_code, equal_to(201))
 
-    #Main part
-    id= response.json()["_id"]
+    # Main part
+    id = response.json()["_id"]
     url = server_url + ":" + server_host + "/v1/books/info/{}".format(id)
     logging.info("URl for GET is :" + url)
     response = get(url)
 
-    assert_that(response.status_code, equal_to( 200))
-    assert_that(response.json()["_title"] == "Twilight")
-    assert_that(response.json()["_book_type"] == "Drama")
-    assert_that(response.json()["_creation_date"] == "2005-01-12")
+    assert_that(response.status_code, equal_to(200))
+    assert_that(response.json()["_title"], equal_to("Twilight"))
+    assert_that(response.json()["_book_type"], equal_to("Drama"))
+    assert_that(response.json()["_creation_date"], equal_to("2005-01-12"))
+
 
 @pytest.mark.get
 @pytest.mark.smoke
@@ -253,7 +282,8 @@ def test_get_book_by_non_existing_id_return_404():
 
     assert_that(response.status_code, equal_to(404))
 
-#Parametrized test
+
+# Parametrized test
 @pytest.fixture(scope="function", params=[
     ("Science Fiction", 201),
     ("Satire", 201),
@@ -270,18 +300,15 @@ def test_get_book_by_non_existing_id_return_404():
 def param_test(request):
     return request.param
 
+
 def test_book_type_values(param_test):
     (book_type, expected_status_code) = param_test
     new_book = {
-        "_book_type":book_type,
-        "_title":"book type test",
-        "_creation_date":"1990-01-12"
+        "_book_type": book_type,
+        "_title": "book type test",
+        "_creation_date": "1990-01-12"
     }
-    url_for_create_book = server_url+":"+server_host+"/v1/books/manipulation/"
+    url_for_create_book = server_url + ":" + server_host + "/v1/books/manipulation/"
     response = post(url_for_create_book, json=new_book)
 
-    assert_that(response.status_code, equal_to( expected_status_code))
-
-
-
-
+    assert_that(response.status_code, equal_to(expected_status_code))
